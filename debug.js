04da378dcb792be06470eaac47eded24c98cc80d
guozidi <100544369+guozidi@users.noreply.github.com>
2026-09-22 00:19:01 +0800

    Update debug.js

--- debug.js
+++ debug.js
@@ -1,3 +1,7 @@
-let cookie = $request.headers['Cookie'] || $request.headers['cookie'] || "";
-$notify("JD Cookie调试", "", "URL: " + $request.url + "\nCookie: " + cookie.substring(0, 200));
+const cookie = $request.headers['Cookie'] || $request.headers['cookie'] || "";
+console.log("=== JD Debug ===");
+console.log("URL: " + $request.url);
+console.log("Cookie长度: " + cookie.length);
+console.log("Cookie内容: " + cookie);
+console.log("包含pt_key: " + cookie.includes("pt_key"));
 $done({});
